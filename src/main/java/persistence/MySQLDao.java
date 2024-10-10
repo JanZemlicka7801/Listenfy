d8ee74dc00496f5d13b748c03e4f023ea5da02b9
smath <smathewslynch@gmail.com>
2024-10-10 06:38:12 +0100

    Created the MySQLDao to easily establish connection to MySQL database using a properties file for configuration

---
+++ src/main/java/persistence/MySQLDao.java
@@ -0,0 +1,54 @@
+package persistence;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.Properties;
+
+public class MySQLDao {
+    private Properties properties;
+    private String databaseName = "listenify";
+
+    public MySQLDao() {
+    }
+
+    public MySQLDao(String propFilename){
+        properties = new Properties();
+        try {
+            //gets the path of database.properties
+            String path = Thread.currentThread().getContextClassLoader().getResource(propFilename).getPath();
+
+            properties.load(new FileInputStream(path));
+        }catch (IOException e){
+            System.out.println("Error while trying to load properties from " + propFilename + ": " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection(){
+        String driver = properties.getProperty("driver");
+        String url = properties.getProperty("url");
+        String database = properties.getProperty("database");
+        String username = properties.getProperty("username");
+        String password = properties.getProperty("password", "");
+
+        try{
+            Class.forName(driver);
+
+            try{
+                Connection con = DriverManager.getConnection(url+database, username, password);
+                return con;
+            } catch (SQLException e) {
+                System.out.println(LocalDateTime.now() + ": SQLException trying to connect to " + url + database);
+                System.out.println(e.getMessage());
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException while trying to load MySQL driver");
+            System.out.println("Error: " + e.getMessage());
+        }
+        return null;
+    }
+}
