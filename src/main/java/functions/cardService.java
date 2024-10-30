266c59f839491b8c6c58216361bc4e3168750103
Jan Zemlicka <jan.zemlicka1@gmail.com>
2024-10-30 17:39:22 +0000

    Added Javadocs.

--- src/main/java/functions/cardService.java
+++ src/main/java/functions/cardService.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 public class cardService {
 
+    /**
+     * Validates if a credit cards number belongs to either Visa or MasterCard.
+     *
+     * @param creditCard The credit card number as a String either with or without spaces.
+     * @return true if the card is either Mastercard or Visa card.
+     */
     public static boolean cardRegister(String creditCard) {
         String sanitizedCard = creditCard.replaceAll("\\s+", "");
 
@@ -14,12 +20,25 @@ public class cardService {
         return isVisa || isMaster;
     }
 
+    //represents a new range of mastercard possible numbers
     public static List<String> MASTERCARD_NEW_RANGE = Arrays.asList("222100","272099");
 
+    /**
+     * Checks if a given credit card number is a valid Mastercard number.
+     *
+     * @param number A credit card number as a String.
+     * @return true if the card is valid Mastercard.
+     */
     public static boolean isBrandMaster(final String number) {
         return number != null && number.length() == 16 && (number.matches("5[1-5][0-9]{14}") || isValidMasterBin(number));
     }
 
+    /**
+     * Checks if a given credit card number is a valid Visa number.
+     *
+     * @param number A credit card number as a String.
+     * @return true if the card is valid Visa.
+     */
     public static boolean isBrandVisa(final String number) {
         if (number == null || number.length() == 0 || (!number.startsWith("4")) || (number.length() != 13 && number.length() != 16)) {
             return false;
@@ -28,6 +47,12 @@ public class cardService {
         return isValidLuhn(number);
     }
 
+    /**
+     * Validates if the first six digits of a Mastercard number fall within new range of BINs.
+     *
+     * @param number The credit card number as a String.
+     * @return true if the first six digits are within the valid Mastercard range.
+     */
     public static boolean isValidMasterBin(String number) {
         if (number.length() < 6) {
             return false;
@@ -41,6 +66,12 @@ public class cardService {
         return parsedNumber >= startingRange && parsedNumber <= endingRange;
     }
 
+    /**
+     * Validates a credit number using Luhn algorithm.
+     *
+     * @param number A credit card number as a String.
+     * @return true if the number is valid according to the Luhn algorithm.
+     */
     public static boolean isValidLuhn(String number) {
         int sum = 0;
         boolean alternate = false;
---
+++ src/test/java/functions/RatingServiceTest.java
@@ -0,0 +1,24 @@
+package functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RatingServiceTest {
+
+    @Test
+    void rateSong() {
+    }
+
+    @Test
+    void viewRatedSongs() {
+    }
+
+    @Test
+    void getTopRatedSong() {
+    }
+
+    @Test
+    void getMostPopularSong() {
+    }
+}
