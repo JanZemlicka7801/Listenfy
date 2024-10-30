eb18ed6f53693f82a3a9d6b3ff1667d8e8249cd8
smath <smathewslynch@gmail.com>
2024-10-30 14:31:37 +0000

    Created the getAlbumsFromArtist method must finish later

--- src/main/java/functions/daoExtentions.java
+++ src/main/java/functions/daoExtentions.java
@@ -1,11 +1,15 @@
 package functions;
 
+import business.Albums;
 import business.Artist;
+import persistence.AlbumDaoImpl;
+import persistence.AlbumsDao;
 import persistence.ArtistDao;
 import persistence.ArtistDaoImpl;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Scanner;
 
 public class daoExtentions {
     public static void viewAllArtists(ArtistDao artistDao) {
@@ -18,5 +22,19 @@ public class daoExtentions {
         } catch (SQLException e) {
             System.out.println("Error fetching artists: " + e.getMessage());
         }
+    }
+    public static void getAlbumsFromArtist(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter artist's first name (leave blank if single name or just has band name): ");
+        String firstName = scanner.nextLine();
+        if (firstName.isEmpty()) {
+            firstName = null;
+        }
+        System.out.print("Enter artist's last name: ");
+        String lastName = scanner.nextLine();
+
+        AlbumsDao albumsDao = new AlbumDaoImpl("database.properties");
+
+        List<Albums> albums = albumsDao.getAlbumsByArtistName(firstName, lastName);
     }
 }
